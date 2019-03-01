83246d9ad0b0e1b3cd56c4e7729ccc78daeedb5c
Michiel, Jeroen <jeroen.michiel@flir.com>
2019-03-01 16:03:37 +0100

    package/python-cheroot: bump to 6.5.4
    needed to upgrade cherrypy to 17.4.1

--- package/python-cheroot/python-cheroot.mk
+++ package/python-cheroot/python-cheroot.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_CHEROOT_VERSION = 5.10.0
+PYTHON_CHEROOT_VERSION = 6.5.4
 PYTHON_CHEROOT_SOURCE = cheroot-$(PYTHON_CHEROOT_VERSION).tar.gz
-PYTHON_CHEROOT_SITE = https://pypi.python.org/packages/14/dc/afc41e7f7e797973808d8520e6aef21c4efd00550aa32f5b726327d36be0
+PYTHON_CHEROOT_SITE = https://files.pythonhosted.org/packages/09/c0/e012219265c83f70b381b4249df50b4a2923bfd9cb0219900e5c2f158c90
 PYTHON_CHEROOT_LICENSE = BSD-3-Clause
 PYTHON_CHEROOT_LICENSE_FILES = LICENSE
 PYTHON_CHEROOT_SETUP_TYPE = setuptools
-PYTHON_CHEROOT_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_CHEROOT_DEPENDENCIES = host-python-setuptools-scm-git-archive
 
 $(eval $(python-package))
