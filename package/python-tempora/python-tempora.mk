17fa611ff6b201502bcae87dab55611e75883cb4
Michiel, Jeroen <jeroen.michiel@flir.com>
2019-03-01 15:14:51 +0100

    package/python-tempora: bump to version 1.8
    needed to allow updating python-portend to 2.3

--- package/python-tempora/python-tempora.mk
+++ package/python-tempora/python-tempora.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TEMPORA_VERSION = 1.7
+PYTHON_TEMPORA_VERSION = 1.8
 PYTHON_TEMPORA_SOURCE = tempora-$(PYTHON_TEMPORA_VERSION).tar.gz
-PYTHON_TEMPORA_SITE = https://pypi.python.org/packages/db/2d/c838e9e553d774962a71b832b2c6390194904bfe49fd0d9d792ce8091e8a
+PYTHON_TEMPORA_SITE = https://files.pythonhosted.org/packages/b4/61/9a4b4e07b8a2a1051ff15fc36083fb9174f8474047fb2822d32b473aedb9
 PYTHON_TEMPORA_LICENSE = MIT
 PYTHON_TEMPORA_SETUP_TYPE = setuptools
 PYTHON_TEMPORA_DEPENDENCIES = host-python-setuptools-scm
